886af7b5b4f2af2d6074919b7c2528484707a93e
Jarred Cianciulli <jcianci1@gmail.com>
2024-07-13 20:47:38 -0400

    third commit

--- routes/jackrabbitRoutes.js
+++ routes/jackrabbitRoutes.js
@@ -1,19 +1,15 @@
 const express = require("express");
+const axios = require("axios");
 
 const router = express.Router();
 
 router.get("/api/data", async (req, res) => {
   try {
-    const response = await fetch(
+    const response = await axios.get(
       "https://app.jackrabbitclass.com/jr3.0/Openings/OpeningsJS?OrgID=506823"
     );
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.text();
-    res.send(data);
+    res.send(response.data);
   } catch (error) {
     console.error("Error fetching data from API:", error.message);
     res.status(500).json({ error: error.message });
